4c91708d2af03292da5df17f4443fde5d4f9592f
devMunyi <samunyi90@gmail.com>
2023-07-14 10:50:49 +0300

    Add Login Design

---
+++ src/main/java/com/example/inventorymanagementsystem/App.java
@@ -0,0 +1,19 @@
+package com.example.inventorymanagementsystem;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/login.fxml"));
+        Scene scene = new Scene(loader.load());
+
+        stage.setScene(scene);
+        stage.setTitle("IMS");
+        stage.show();
+    }
+}
